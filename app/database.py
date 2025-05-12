c699cdcf0ecd299a18458032b74ceaaee1bebafd
Jaime_Pulido <jpuldia043@g.educaand.es>
2025-05-12 17:55:15 +0200

    Prueba Linter de nuevo 3º intento

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlmodel import create_engine, SQLModel
 
 SQLITE_FILE_NAME = "db.sqlite3"
 
-engine = create_engine(f"sqlite:///{SQLITE_FILE_NAME}", echo=True)
+engine = create_engine(f"sqlite:///{SQLITE_FILE_NAME}", echo=True) 
 
 
 def create_db_and_tables():
